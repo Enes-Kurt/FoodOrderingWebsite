be0158ce3dd7cb465c60f3c0db9ec1fa321e9db2
HTA <tgakkkaya@hotmail.com>
2023-07-12 11:55:26 +0300

    Add project files.

---
+++ MVCFoodShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCFoodShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
