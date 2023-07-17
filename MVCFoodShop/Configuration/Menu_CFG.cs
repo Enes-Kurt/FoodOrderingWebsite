4b018a31fdc84ff8f3cdcd6e15888c888a428e82
HTA <tgakkkaya@hotmail.com>
2023-07-17 12:42:21 +0300

    1707-1241

--- MVCFoodShop/Configuration/Menu_CFG.cs
+++ MVCFoodShop/Configuration/Menu_CFG.cs
@@ -8,40 +8,56 @@ namespace MVCFoodShop.Configuration
     {
         public void Configure(EntityTypeBuilder<Menu> builder)
         {
-            builder.HasData(
-                new Menu { 
-                    ID = 1, 
-                    MenuName = "Whopper", 
-                    MenuPrice = 180, 
-                    MenuIsActive = true, 
-                    MenuDeclaration= "Wooper Menu offers the king of flavors! A juicy and delicious beef patty, fresh vegetables, and mouthwatering sauces combined in one burger experience.",
-                    MenuCoverImage= "double-whopper-menu.png"
-                },
-                new Menu { 
-                    ID = 2, 
-                    MenuName = "Big King", 
-                    MenuPrice = 170, 
-                    MenuIsActive = true,
-                    MenuDeclaration = "Big Kink, a burger that's larger than life! Juicy beef patty, melted cheese, crispy bacon, and tangy special sauce come together in this epic burger indulgence.",
-                    MenuCoverImage = "big-king-menu.png"
-                },
-                new Menu { 
-                    ID = 3, 
-                    MenuName = "King Chicken", 
-                    MenuPrice = 160, 
-                    MenuIsActive = true,
-                    MenuDeclaration = "King Chicken, a royal treat for chicken lovers! Crispy, golden-brown chicken patty, fresh lettuce, and creamy mayo unite in a sandwich fit for a king.",
-                    MenuCoverImage = "bk-crispy-chicken-menu.png"
-                },
-                new Menu { 
-                    ID = 4, 
-                    MenuName = "Kids Menu", 
-                    MenuPrice = 140, 
-                    MenuIsActive = true,
-                    MenuDeclaration = "Kids Menu, a delightful feast for our little foodies! Mini burger, crispy fries, and a refreshing drink, specially crafted to satisfy their appetites and bring smiles to their faces.",
-                    MenuCoverImage = "kids-hamburger (1).png"
-                }
-                );
+            //builder.HasData(
+            //    new Menu
+            //    {
+            //        ID = 1,
+            //        MenuName = "Whopper",
+            //        MenuPrice = 180,
+            //        MenuIsActive = true,
+            //        FoodCount = 1,
+            //        BeverageCount = 1,
+            //        SauceCount = 2,
+            //        MenuDeclaration = "Wooper Menu offers the king of flavors! A juicy and delicious beef patty, fresh vegetables, and mouthwatering sauces combined in one burger experience.",
+            //        MenuCoverImage = "double-whopper-menu.png"
+            //    },
+            //    new Menu
+            //    {
+            //        ID = 2,
+            //        MenuName = "Big King",
+            //        MenuPrice = 170,
+            //        MenuIsActive = true,
+            //        FoodCount = 1,
+            //        BeverageCount = 1,
+            //        SauceCount = 2,
+            //        MenuDeclaration = "Big Kink, a burger that's larger than life! Juicy beef patty, melted cheese, crispy bacon, and tangy special sauce come together in this epic burger indulgence.",
+            //        MenuCoverImage = "big-king-menu.png"
+            //    },
+            //    new Menu
+            //    {
+            //        ID = 3,
+            //        MenuName = "King Chicken",
+            //        MenuPrice = 160,
+            //        MenuIsActive = true,
+            //        FoodCount = 1,
+            //        BeverageCount = 1,
+            //        SauceCount = 2,
+            //        MenuDeclaration = "King Chicken, a royal treat for chicken lovers! Crispy, golden-brown chicken patty, fresh lettuce, and creamy mayo unite in a sandwich fit for a king.",
+            //        MenuCoverImage = "bk-crispy-chicken-menu.png"
+            //    },
+            //    new Menu
+            //    {
+            //        ID = 4,
+            //        MenuName = "Kids Menu",
+            //        MenuPrice = 140,
+            //        MenuIsActive = true,
+            //        FoodCount = 1,
+            //        BeverageCount = 1,
+            //        SauceCount = 2,
+            //        MenuDeclaration = "Kids Menu, a delightful feast for our little foodies! Mini burger, crispy fries, and a refreshing drink, specially crafted to satisfy their appetites and bring smiles to their faces.",
+            //        MenuCoverImage = "kids-hamburger (1).png"
+            //    }
+            //    );
         }
     }
 }
--- MVCFoodShop/wwwroot/js/Products.js
+++ MVCFoodShop/wwwroot/js/Products.js
@@ -41,7 +41,7 @@ function AddToShoppingCart(productOrMenuID, typeName, count) {
 function AddMenuToShoppingCart(menuID,count2) {
     var elementAmount1 = document.getElementById('menuOrderCount' + count2).value;
     var menuType = document.getElementById('menuType' + count2).value;
-    var menuCartElements = document.getElementsByName('menuCartElement');
+    var menuCartElements = document.getElementsByName('menuCartElement' + count2);
     var selectedValues = [];
     for (var i = 0; i < menuCartElements.length; i++) {
         var selectedValue = menuCartElements[i].value;
